047abb6ff49c363cd843de61a9c93b6d054b36e5
Phan Nhật Tân <162537750+nhattan2005@users.noreply.github.com>
2025-03-04 00:47:05 +0700

    Add files via upload

--- do_eval.sh
+++ do_eval.sh
@@ -4,6 +4,6 @@ video_feat_name=$3
 gt_file_name=$4
 
 python evaluation.py --local_rank=0 \
-    --text_feat_path="data/datasets/$test_collection/FeatureData/$text_feat_name" \
-    --video_feat_path="data/datasets/$test_collection/FeatureData/$video_feat_name" \
-    --gt_file_path="data/datasets/$test_collection/Annotations/$gt_file_name.txt"
+    --text_feat_path="/home/nhattanpro444/Project/TeachCLIP/reproduce_TeachCLIP/data/datasets/$test_collection/FeatureData/$text_feat_name/xclip/xclip/xclip" \
+    --video_feat_path="/home/nhattanpro444/Project/TeachCLIP/reproduce_TeachCLIP/data/datasets/$test_collection/FeatureData/$video_feat_name/xclip/xclip/xclip" \
+    --gt_file_path="/home/nhattanpro444/Project/TeachCLIP/reproduce_TeachCLIP/data/datasets/msrvtt/Annotations/$gt_file_name.txt"
--- do_extract_text_feat.sh
+++ do_extract_text_feat.sh
@@ -7,10 +7,10 @@ config=$(echo "$model_name" | cut -d'/' -f3)
 run=$(echo "$model_name" | cut -d'/' -f4)
 
 python -m torch.distributed.launch extract_feat.py --datatype text \
-    --local_rank=3 --num_thread_reader=8 --batch_size=100 \
+    --local_rank=3 --num_thread_reader=8 --batch_size=8 \
     --queryfile_path "data/datasets/$test_collection/QuerySet/$queryset.txt" \
     --output_dir "data/datasets/$test_collection/FeatureData/$queryset/$train_collection/$config/$run" \
-    --init_model "data/datasets/$model_name/best_model.bin" \
+    --init_model "/home/nhattanpro444/Project/TeachCLIP/reproduce_TeachCLIP/data/teacher_checkpoints/$model_name/best_model.bin" \
     --max_frames=12 --max_words=32 --feature_framerate 1 --slice_framepos 2 \
     --linear_patch 2d --sim_header seqTransf \
     --pretrained_clip_name ViT-B/32
